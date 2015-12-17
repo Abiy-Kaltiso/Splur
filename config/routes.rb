73152d47a9a0f3bd1879c8a2e53ce904d69c10ab
Abiy-Kaltiso <abiyk7@gmail.com>
2015-12-17 22:45:48 +0000

    added page to add new quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Splurty::Application.routes.draw do
+   root 'quotes#index'
+
+  resources :quotes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -52,6 +55,5 @@ Splurty::Application.routes.draw do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
-  root 'quotes#index'
   #   end
 end
